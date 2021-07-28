9a23f3878df5cd8b47c41558b6f41de23d7cb46b
louisjmorgan <louis.morgan@hotmail.com>
2021-07-28 21:17:21 +0100

    added babel-loader to webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import './styles.css';
 
-const dropDownBtn = document.querySelector('.dropDownBtn');
-const dropDownList = document.querySelector('.dropDownList');
-dropDownBtn.addEventListener('click', () => {
-    dropDownList.style.display = 'block';
-});
